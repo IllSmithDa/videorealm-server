82b4a0879be3abf35396efc0a073c9342acbfca1
Samuel Kim <sams404link@gmail.com>
2018-07-19 20:25:33 -0700

    switched to production server

--- reqURL.js
+++ reqURL.js
@@ -1,8 +1,8 @@
 // local environment testing 
-const reqURL='http://localhost:3000';
+// const reqURL='http://localhost:3000';
 
 // heroku environment
-// const reqURL='https://friendrealm.herokuapp.com';
+const reqURL='https://friendrealm.herokuapp.com';
 
 module.exports = {
   reqURL
