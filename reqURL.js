79a8fb4231b0e4b29c8c0eeaa81e286d6437b9e5
Samuel Kim <sams404link@gmail.com>
2018-07-19 18:30:54 -0700

    updated to production

--- reqURL.js
+++ reqURL.js
@@ -1,5 +1,5 @@
 // local environment testing 
-const reqURL='http://localhost:3000';
+// const reqURL='http://localhost:3000';
 
 // heroku environment
 const reqURL='https://friendrealm.herokuapp.com';
