86fade4fcdbc5ff9d8f97b1c0ac969a5f1ecdd94
Samuel Kim <sams404link@gmail.com>
2018-07-19 18:25:33 -0700

    added new buildpacks

--- reqURL.js
+++ reqURL.js
@@ -2,8 +2,8 @@
 const reqURL='http://localhost:3000';
 
 // heroku environment
-// const reqURL='https://friendrealm.herokuapp.com';
+const reqURL='https://friendrealm.herokuapp.com';
 
 module.exports = {
-  reqURL,
+  reqURL
 }
