f86624d32a33d27419c4ea5b29e535ceb390557c
Samuel Kim <sams404link@gmail.com>
2018-07-27 00:26:52 -0700

    added url req from angular app

--- reqURL.js
+++ reqURL.js
@@ -1,8 +1,11 @@
-// local environment testing 
+// local react env 
 // const reqURL='http://localhost:3000';
 
+// local angular env
+const reqURL='http://localhost:4200';
+
 // heroku environment
-const reqURL='https://friendrealm.herokuapp.com';
+// const reqURL='https://friendrealm.herokuapp.com';
 
 module.exports = {
   reqURL
