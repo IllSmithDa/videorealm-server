bf801abfe28c3f8ae847bc116c15e44a5374b641
Samuel Kim <sams404link@gmail.com>
2018-08-13 23:42:42 -0700

    production

--- reqURL.js
+++ reqURL.js
@@ -1,11 +1,11 @@
 // local react env 
-const reqURL='http://localhost:3000';
+// const reqURL='http://localhost:3000';
 
 // local angular env
 // const reqURL='http://localhost:4200';
 
 // heroku environment
-// const reqURL='https://videorealm.herokuapp.com';
+const reqURL='https://videorealm.herokuapp.com';
 
 module.exports = {
   reqURL
